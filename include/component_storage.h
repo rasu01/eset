53c4feebed4fa69dfcc6ec2e02461cdce9b540b3
Rasmus Hallbäck <rasmushallback01@gmail.com>
2022-05-28 00:19:40 +0200

    component storage insert, remove and end copy

--- include/component_storage.h
+++ include/component_storage.h
@@ -3,19 +3,41 @@
 
 namespace bunshi {
 
-    //a strorage used for a single component inside a molecule
+    //a stack based strorage container used for a single component inside a molecule
     class ComponentStorage {
         public:
 
             ComponentStorage() = default;
-            ComponentStorage(size_t component_size, size_t start_size);
+            ComponentStorage(size_t component_size);
             ~ComponentStorage();
 
+            /*
+                Get the pointer to a component from an Entity's offset.
+            */
             void* get_component_pointer(size_t offset);
-            void swap_by_offsets(size_t a, size_t b);
+
+            /*
+                Copies the last component to another offset.
+                Very useful when removing an entity's components from one
+                molecule(archetype).
+            */
+            void copy_end(size_t desination_offset);
+
+            /*
+                Inserts data from a component into the storage at the end.
+            */
+            void insert_end(void* pointer);
+
+            /*
+                Removes the last component.
+                In reality, it just decreases the component_count by one.
+            */
+            void remove_end();
+
         private:
             uint8_t* data;
             size_t size;
+            size_t component_count;
             size_t component_size;
 
             /*
--- src/component_storage.cpp
+++ src/component_storage.cpp
@@ -3,10 +3,11 @@
 
 using namespace bunshi;
 
-ComponentStorage::ComponentStorage(size_t a_component_size, size_t a_start_size) {
+ComponentStorage::ComponentStorage(size_t a_component_size) {
     component_size = a_component_size;
-    size = a_start_size;
+    size = a_component_size * 10; //default size is to atleast make sure 10 components fits inside
     data = (uint8_t*)malloc(size);
+    component_count = 0;
 }
 
 ComponentStorage::~ComponentStorage() {
@@ -14,9 +15,28 @@ ComponentStorage::~ComponentStorage() {
 }
 
 void* ComponentStorage::get_component_pointer(size_t offset) {
-    return 0;
+    return data + offset*component_size;
 }
 
+void ComponentStorage::copy_end(size_t desination_offset) {
+    memcpy(data + desination_offset*component_size, data + component_count*component_size, component_size);
+}
+
+void ComponentStorage::insert_end(void* pointer) {
+
+    //if we are trying to overwrite the data, resize it before copying
+    if(component_count * component_size > size) {
+        resize();
+    }
+    memcpy(data + component_count * component_size, (uint8_t*)pointer, component_size);
+    component_count++;
+}
+
+void ComponentStorage::remove_end() {
+    component_count--;
+}
+
+//private
 void ComponentStorage::resize() {
     if(data) {
         uint8_t* new_data = (uint8_t*)malloc(size * 2);
