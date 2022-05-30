f357ca21a3eb5553cf5db4056212a9226d9c0c03
Rasmus Hallbäck <rasmushallback01@gmail.com>
2022-05-30 23:50:45 +0200

    testing the exist function

--- test/testing.cpp
+++ test/testing.cpp
@@ -4,7 +4,14 @@
 
 int main() {
 
-    //bunshi::Universe universe;
+    bunshi::Universe universe;
+
+    bunshi::Entity entity = 1;
+    if(!universe.exist(entity)) {
+        std::cout << "Passed!\n";
+    } else {
+        std::cout << "Failed!\n";
+    }
 
     std::cout << typeid(bunshi::Entity).hash_code() << ", " << typeid(size_t).hash_code() << "\n";
 
